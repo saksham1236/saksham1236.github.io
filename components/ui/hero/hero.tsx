5a6846fdd510abb66b36ac859323c93960086945
Saksham <saksham1236@gmail.com>
2025-04-20 20:19:31 -0400

    Adding animation for hero

--- components/ui/hero/hero.tsx
+++ components/ui/hero/hero.tsx
@@ -1,37 +1,34 @@
 'use client';
 import React from 'react'
 import styles from './hero.module.css';
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+import SplitType from 'split-type';
 
 gsap.registerPlugin(useGSAP)
 
 export default function Hero() {
-    const {contextSafe} = useGSAP();
-    const red = useRef<HTMLSpanElement>(null);
-
-    const onEnter = contextSafe(({ currentTarget }: { currentTarget: EventTarget & HTMLElement }) => {
-        gsap.to(currentTarget, 
-            {
-                scrollTrigger: {
-                  start: "top 80%",
-                  end: "top 30%",
-                  scrub: true,
-                },
-                opacity: 0,
-                y: 50,
-                duration: 2,
-              },
-        );
-      });
+    const container = useRef<HTMLDivElement>(null);
+    useGSAP(() => {
+        const text = new SplitType('.animate', { types: 'lines' });
+        gsap.from(text.lines, {
+            y: '100%',
+            opacity: 0,
+            duration: 0.35,
+            ease: 'none.out',
+            stagger: 0.1,
+            delay: 1
+        });
+    });
     
     return (
-        <div className="hero mt-6">
-            <h1 className="sm:text-md md:text-lg  mb-4">Made by Saksham Tandon.</h1>
-            <h2 className="text-4xl/12 2xl:text-5xl/16 mb-8 font-bold max-w-300">
-                I craft engaging experiences through <span onMouseOver={onEnter} className={`${styles.hoverText} ${styles.red}`}>UX Design</span>, specializing in <span className={styles.hoverText}>Web Development</span> to bring ideas to life, and drive results with a passion for <span className={styles.hoverText}>Data Science.</span>
+        <div className="hero mt-6" ref={container}>
+            <h1 className="sm:text-md md:text-lg mb-4 animate">Made by Saksham Tandon.</h1>
+            <h2 className="text-4xl/12 2xl:text-5xl/16 mb-8 font-bold max-w-300 animate">
+                I craft engaging experiences through <span className={`${styles.hoverText} ${styles.red}`}>UX Design</span>, specializing in <span className={styles.hoverText}>Web Development</span> to bring ideas to life, and drive results with a passion for <span className={styles.hoverText}>Data Science.</span>
             </h2>
         </div>
     )
 }
+
