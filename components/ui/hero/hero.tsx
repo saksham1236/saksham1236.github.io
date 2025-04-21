31f233a0042f1173558bf3fdb2ee2faeb31328af
Saksham <saksham1236@gmail.com>
2025-04-20 21:27:58 -0400

    removing unused useRef

--- components/ui/hero/hero.tsx
+++ components/ui/hero/hero.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import styles from './hero.module.css';
-import { useRef, useEffect } from 'react';
+import { useRef } from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import SplitType from 'split-type';
