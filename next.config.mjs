9ea3f420a978eddaaecd90b042d67d0a90ccbc5f
Saksham <saksham1236@gmail.com>
2025-04-21 23:36:26 -0400

    Removing Static Export

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/documents-simple-next-js-documentation",
-    output:"export",
-    reactStrictMode: true,
 }
 
 export default nextConfig
