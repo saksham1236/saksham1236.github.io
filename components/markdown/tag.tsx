4109fcf6fa471713775ffdd392208cd57b544c9c
Saksham <saksham1236@gmail.com>
2025-05-05 00:58:39 -0400

    Fixing empty object params

--- components/markdown/tag.tsx
+++ components/markdown/tag.tsx
@@ -1,4 +1,4 @@
-export default function Tag({ children }: React.PropsWithChildren<{}>) {
+export default function Tag({ children }: React.PropsWithChildren) {
     return (
         <span className="tag h-16 inline-flex flex-row items-center gap-1 rounded-xl bg-sheet-primary px-2 py-1 mr-2 mb-2 text-lg font-semibold !text-primary">
             {children}
