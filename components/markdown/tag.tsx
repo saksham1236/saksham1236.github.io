37b04c5fabfb17a493046cc3662c4789281122e2
Saksham <saksham1236@gmail.com>
2025-05-10 14:39:27 -0400

    Adding new classes for breadcrumbs

--- components/markdown/tag.tsx
+++ components/markdown/tag.tsx
@@ -1,6 +1,6 @@
 export default function Tag({ children }: React.PropsWithChildren) {
     return (
-        <span className="tag h-16 inline-flex flex-row items-center gap-1 rounded-xl bg-sheet-primary px-2 py-1 mr-2 mb-2 text-lg font-semibold !text-primary">
+        <span className="tag h-12 inline-flex flex-row items-center gap-1.5 rounded-xl bg-sheet-primary px-3 mr-2 mb-2 text-md font-semibold !text-primary">
             {children}
         </span>
     )
--- components/ui/breadcrumb.tsx
+++ components/ui/breadcrumb.tsx
@@ -6,7 +6,7 @@ import { LuChevronRight } from "react-icons/lu"
 import { cn } from "@/lib/utils"
 
 function Breadcrumb({ ...props }: React.ComponentProps<"nav">) {
-  return <nav aria-label="breadcrumb" data-slot="breadcrumb" {...props} />
+  return <nav className="overflow-auto bg-sheet-primary rounded-md" aria-label="breadcrumb" data-slot="breadcrumb" {...props} />
 }
 
 function BreadcrumbList({ className, ...props }: React.ComponentProps<"ol">) {
@@ -14,7 +14,7 @@ function BreadcrumbList({ className, ...props }: React.ComponentProps<"ol">) {
     <ol
       data-slot="breadcrumb-list"
       className={cn(
-        "text-muted-foreground flex flex-wrap items-center gap-0.5 text-sm text-pretty break-words sm:gap-0.5",
+        "text-muted-foreground flex items-center gap-0.5 text-sm text-pretty sm:gap-0.5",
         className
       )}
       {...props}
@@ -26,7 +26,7 @@ function BreadcrumbItem({ className, ...props }: React.ComponentProps<"li">) {
   return (
     <li
       data-slot="breadcrumb-item"
-      className={cn("inline-flex w-max items-center gap-0.5 rounded-sm", className)}
+      className={cn("inline-flex items-center gap-0.5 rounded-sm", className)}
       {...props}
     />
   )
@@ -44,7 +44,7 @@ function BreadcrumbLink({
   return (
     <Comp
       data-slot="breadcrumb-link"
-      className={cn("p-2 hover:text-foreground hover:bg-hover-b rounded-sm transition-colors", className)}
+      className={cn("p-2 text-nowrap hover:text-foreground hover:bg-hover-b rounded-sm transition-colors", className)}
       {...props}
     />
   )
@@ -57,7 +57,7 @@ function BreadcrumbPage({ className, ...props }: React.ComponentProps<"span">) {
       role="link"
       aria-disabled="true"
       aria-current="page"
-      className={cn("p-2 text-foreground font-normal", className)}
+      className={cn("p-2 text-nowrap text-foreground font-normal", className)}
       {...props}
     />
   )
