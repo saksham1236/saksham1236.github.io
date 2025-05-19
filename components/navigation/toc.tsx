f363a2881849aacb9b03736819dc21aae646cca4
Saksham <saksham1236@gmail.com>
2025-05-18 20:21:48 -0400

    Adding styles for l4 in toc

--- components/navigation/toc.tsx
+++ components/navigation/toc.tsx
@@ -28,23 +28,23 @@ export default function Toc({ tocs }: TocProps) {
   return (
     <div className="bg-secondary flex w-full overflow-y-hidden flex-col gap-3 rounded-lg p-3 dark:bg-[#00000033]">
       <h3 className="text-sm font-semibold">On this page</h3>
-        <div className="link-map overflow-y-auto bg-sheet-primary flex flex-col rounded-sm text-sm text-neutral-800 dark:text-neutral-300/85">
-          {tocs.map(({ href, level, text }) => (
-            <Link
-              key={href}
-              href={href}
-              scroll={false}
-              onClick={(e) => handleSmoothScroll(e, href)}
-              className={clsx({
-                "pl-0 pb-1 l2 border-b-1 border-ring": level == 2,
-                "text-sm pl-2 py-1.5 l3": level == 3,
-                "text-xs pl-4 l4": level == 4,
-              })}
-            >
-              <span className = "link-inner">{text}</span>
-            </Link>
-          ))}
-        </div>
+      <div className="link-map overflow-y-auto bg-sheet-primary flex flex-col rounded-sm text-sm text-neutral-800 dark:text-neutral-300/85">
+        {tocs.map(({ href, level, text }) => (
+          <Link
+            key={href}
+            href={href}
+            scroll={false}
+            onClick={(e) => handleSmoothScroll(e, href)}
+            className={clsx({
+              "pl-0 pb-1 l2 border-b-1 border-ring": level == 2,
+              "text-sm pl-2 py-1.5 l3": level == 3,
+              "pl-4 border-l l4": level == 4,
+            })}
+          >
+            {text}
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
