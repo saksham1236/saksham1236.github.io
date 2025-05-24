7e920614361c7f01cff11d4bb2cae695c91d2b72
Saksham <saksham1236@gmail.com>
2025-05-24 16:08:52 -0400

    Removing external links to fix build error

--- dist/scripts/settings/documents.mjs
+++ dist/scripts/settings/documents.mjs
@@ -1,7 +1,7 @@
 export const Documents = [
     {
         heading: "My Work",
-        title: "Recent Work",
+        title: "Featured Work",
         href: "/recent-work",
         items: [
             {
@@ -20,6 +20,58 @@ export const Documents = [
                 title: "Mind Games",
                 href: "/mind-games",
             },
+        ],
+    },
+    {
+        title: "Scientific Illustration Work",
+        href: "/recent-work#scientific-illustration-work",
+        items: [
+            {
+                title: "Rydberg Atom Quantum Simulator",
+                href: "https://www.behance.net/gallery/202608739/Rydberg-Atom-Quantum-Simulator-Diagrams",
+            },
+            {
+                title: "Journal Cover - Biophysical properties of tear film lipid layer II. Polymorphism of FAHFA",
+                href: "https://www.cell.com/biophysj/issue?pii=S0006-3495(21)X0003-3#fullCover",
+            },
+            {
+                title: "Journal Cover Submission - Airborne Transmission of COVID-19",
+                href: "https://www.behance.net/gallery/111582107/ACS-Nano-Cover-submission",
+            },
+            {
+                title: "Color Blindness Animation",
+                href: "https://www.youtube.com/watch?v=iBTqyTwAWig&t=2s",
+            },
+            {
+                title: "Atomic Force Microscopy Setup",
+                href: "https://www.behance.net/gallery/113015787/Scientific-Illustration-University-of-Hawaii-intern",
+            },
+            {
+                title: "Journal Cover - Atomic Force Microscopy Imaging of Adsorbed Pulmonary Surfactant Films",
+                href: "https://www.cell.com/biophysj/issue?pii=S0006-3495(19)X0017-X#fullCover",
+            }
+        ],
+    },
+    {
+        title: "Dev Work",
+        href: "/recent-work#dev-work",
+        items: [
+            {
+                title: "GlassDoor Salary Data Analysis",
+                href: "https://github.com/saksham1236/Data-Salary-Analysis",
+            },
+            {
+                title: "BinThere",
+                href: "https://github.com/saksham1236/industry-project-st-fork",
+            },
+            {
+                title: "Layoff Warning System",
+                href: "https://github.com/saksham1236/saksham-tandon-capstone-lws-front-end",
+            },
+            {
+                title: "Instock",
+                href: "https://github.com/saksham1236/brnstn_instock-mp2-frontend",
+            }
         ],
     },
     {
