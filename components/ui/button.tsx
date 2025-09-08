d6d6dc4d067664366ba044c7d610c0b7373540b1
Saksham Tandon <saksham1236@gmail.com>
2025-09-07 22:50:29 -0400

    removing shadows from buttons

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,13 +10,13 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary/90 text-primary-foreground shadow-xs hover:bg-primary hover:outline-2 hover:outline-offset-2 hover:outline-solid",
+          "bg-primary/90 text-primary-foreground hover:bg-primary hover:outline-2 hover:outline-offset-2 hover:outline-solid",
         destructive:
-          "bg-destructive/90 text-white shadow-xs hover:bg-destructive focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+          "bg-destructive/90 text-white hover:bg-destructive focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+          "border bg-background hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
-          "bg-secondary/50 text-secondary-foreground shadow-xs hover:bg-secondary/80",
+          "bg-secondary/50 text-secondary-foreground hover:bg-secondary/80",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
