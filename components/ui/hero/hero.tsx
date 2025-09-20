699fd4847b320d4cce54929626a2e2ca6571405e
Saksham Tandon <saksham1236@gmail.com>
2025-09-20 04:01:17 -0400

    fixing spelling for hero

--- components/ui/hero/hero.tsx
+++ components/ui/hero/hero.tsx
@@ -34,7 +34,7 @@ export default function Hero() {
       <div className="flex flex-col gap-12 md:gap-6 items-center md:items-start">
         <DioraDesignLogo className="invisible greeting fade-up w-32 h-auto text-white opacity-50 mb-4"/>
         <h1 className="flex invisible greeting fade-up items-center gap-2 text-xl md:text-5xl fade-in-translate-full">Hi I am Saksham.<Emoji className="size-12 md:size-18" emoji="👋" /></h1>
-        <h2 className={cn("headline text-4xl md:text-8xl leading-tight invisible")}>I build enganging <b>Experiences</b>,<br/>to deliver <b>impact.</b></h2>
+        <h2 className={cn("headline text-4xl md:text-8xl leading-tight invisible")}>I build engaging <b>Experiences</b>,<br/>to deliver <b>impact.</b></h2>
       </div>
     </div>
   )
